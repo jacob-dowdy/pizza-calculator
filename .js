9f87f1e8eb3192093953162ac0e1c3ff32327b8c
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-01-21 22:13:16 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,21 @@
+var orderCount = 0;
+function takeOrder(crustType, topping) {
+  orderCount = orderCount + 1;
+}
+
+function getSubTotal(itemCount) {
+  return itemCount * 7.5;
+}
+function 
+getTax() {
+  return getSubTotal(orderCount) * 0.06;
+}
+function
+getTotal() {
+  return getSubTotal(orderCount) + getTax();
+}
+takeOrder('thin', 'mushroom');
+takeOrder('cheese-filled', 'spinach');
+takeOrder('thin', 'sausage');
+console.log(getSubTotal(orderCount));
+console.log(getTotal());
